700d3e88c44d54ed1233a90eaddf7e1d75c4eaa7
Mariano <hobosarefriends@hotmail.com>
2020-04-13 14:37:42 +0200

    Added command-line-options, better logging, refactored a bit

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ A small script for turning babashka scripts into native binaries
 
 ## Requirements
 
-   - JVM
+   - JVM (I think?)
    - babashka
+   - GraalVM
 
 ## Usage
 
   From the nativity directory: `bb nativity.clj path/to/script`
   or even just `./nativity.clj path/to/script`
+
+### Options
+  You can specify which of the built in bb dependencies to require by using the `/nativity.clj path/to/script -d io,str,json`.
+  If you use `-d all` it will include all of them.
+
+  You can rename the output binary by using the -n flag `/nativity.clj path/to/script -n name-of-binary`
--- nativity.clj
+++ nativity.clj
@@ -1,76 +1,129 @@
 #!/usr/bin/env bb
 
-; (def cli-options
-;   [["-o" "--object RANGE" "object range ( min-max, example: 1-40)"
-;     :default (range 1 40)
-;     :parse-fn range-parser]
-;    ; ["-f" "--frame RANGE" "frame range ( min-max, example: 1-40)"
-;    ;  :default (range 1 901)
-;    ;  :parse-fn range-parser]
-;    ; ["-n" "--name FILENAME" "override default file-name"
-;    ;  :default "foo"]
-;    ])
-;
-; (def options (:options (clojure.tools.cli/parse-opts *command-line-args* cli-options)))
+(ns nativity
+  (:require [clojure.string :refer [split] :as str]
+            [clojure.java.io :refer [make-parents delete-file]]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:import [java.lang ProcessBuilder$Redirect]))
 
-(def file-path (first *command-line-args*))
-(def file (-> file-path (str/split #"/") last))
-(def file-name (-> file (str/split #"\.") first))
+
+(defn dependency-parser [input-string] (map keyword (split input-string #",")))
+(def cli-options
+  [["-d" "--deps LIST" "dependency list ( comma seperated values, example: \"io,str,json,edn\")"
+    :default []
+    :parse-fn dependency-parser]
+   ["-n" "--name FILENAME" "override default file name for the binary (will default to the input file name )"
+    :default nil]
+    ["-k" "-keep-files" "keep the generated intermediate files"]
+   ])
+
+(def command-line-input (parse-opts *command-line-args* cli-options))
+(def options (:options command-line-input))
+
+(def file-path (first (:arguments command-line-input)))
+(def file (-> file-path (split #"/") last))
+(def file-name (-> file (split #"\.") first))
 (def name-space file-name)
-(def native-image-name file-name)
+(def native-image-name (or (:name options) file-name))
+(def dependency-keys (:deps options))
 
-(println "reading file")
-(def main-file (slurp file))
+
+;shamelessly copied
+(defn- shell-command
+  ([args] (shell-command args nil))
+  ([args {:keys [:throw?]
+          :or {throw? true}}]
+   (let [pb (let [pb (ProcessBuilder. ^java.util.List args)]
+              (doto pb
+                (.redirectInput ProcessBuilder$Redirect/INHERIT)
+                (.redirectOutput ProcessBuilder$Redirect/INHERIT)
+                (.redirectError ProcessBuilder$Redirect/INHERIT)))
+         proc (.start pb)
+         exit-code (.waitFor proc)]
+     (when (and throw?
+                (not (zero? exit-code)))
+       (throw (ex-info "Got non-zero exit code" {:status exit-code})))
+     {:exit exit-code})))
 
 (def deps-file
-  {:deps {'cheshire {:mvn/version "5.10.0"}
-        'org.clojure/tools.cli {:mvn/version "1.0.194"}
-        'org.clojure/clojure {:mvn/version "1.10.2-alpha1"}
-        'org.clojure/core.async {:mvn/version "1.1.587"}
-        'com.cognitect/transit-clj {:mvn/version "1.0.324"}
-        'bencode {:mvn/version "0.2.5"}
-        'org.clojure/data.csv {:mvn/version "1.0.0"}}
-  :aliases {:native-image
-           {:main-opts [(str "-m clj.native-image " name-space)
-                        "--initialize-at-build-time"
-                        ;; optional native image name override
-                        (str "-H:Name=" native-image-name)]
-            :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
-            :extra-deps
-            {'clj.native-image
-             {:git/url "https://github.com/taylorwood/clj.native-image.git"
-              :sha "7708e7fd4572459c81f6a6b8e44c96f41cdd92d4"}}}}})
+ {:deps {'cheshire {:mvn/version "5.10.0"}
+       'org.clojure/tools.cli {:mvn/version "1.0.194"}
+       'org.clojure/clojure {:mvn/version "1.10.2-alpha1"}
+       'org.clojure/core.async {:mvn/version "1.1.587"}
+       'com.cognitect/transit-clj {:mvn/version "1.0.324"}
+       'bencode {:mvn/version "0.2.5"}
+       'org.clojure/data.csv {:mvn/version "1.0.0"}}
+ :aliases {:native-image
+          {:main-opts [(str "-m clj.native-image " name-space)
+                       "--initialize-at-build-time"
+                       ;; optional native image name override
+                       (str "-H:Name=" native-image-name)]
+           :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+           :extra-deps
+           {'clj.native-image
+            {:git/url "https://github.com/taylorwood/clj.native-image.git"
+             :sha "7708e7fd4572459c81f6a6b8e44c96f41cdd92d4"}}}}})
 
-(def compile-to-native-command "clj -A:native-image")
+
+(def dependency-map
+  '{ :str [clojure.string :as str]
+     :set [clojure.set :as set]
+     :edn [clojure.edn :as edn]
+     :shell [clojure.java.shell :as shell]
+     :io [clojure.java.io :as io]
+     :async [clojure.core.async :as async]
+     :stacktrace [clojure.stacktrace]
+     :test [clojure.test]
+     :pprint [clojure.pprint :as pprint]
+     :cli [clojure.tools.cli :as cli]
+     :csv [clojure.data.csv :as csv]
+     :json [cheshire.core :as json]
+     :transit [cognitect.transit :as transit]
+     :bencode [bencode.core :as bencode]})
+
+
+(defn specific-dependencies [keylist]
+ (cond
+    (some #{:all} keylist) (conj (vals dependency-map) :require)
+    (empty? keylist) nil
+    :else (conj (vals (select-keys dependency-map keylist)) :require)))
+
+(def compile-to-native-command ["clj" "-A:native-image" "--no-fallback"] )
+
+(println "reading file")
+(def main-file (slurp file-path))
+
 (def src-output-path (str "src/" file-name ".clj"))
 
 (println "making directory")
-(io/make-parents src-output-path)
+(make-parents src-output-path)
 
 (println "making modified script-file")
-(spit src-output-path
-  (str
-    (conj
-        '((:gen-class)
-          (:require [clojure.string :as str]
-              [clojure.set :as set]
-              [clojure.edn :as edn]
-              [clojure.java.shell :as shell]
-              [clojure.java.io :as io]
-              [clojure.core.async :as async]
-              [clojure.stacktrace]
-              [clojure.test]
-              [clojure.pprint :as pprint]
-              [clojure.tools.cli :as cli]
-              [clojure.data.csv :as csv]
-              [cheshire.core :as json]
-              [cognitect.transit :as transit]
-              [bencode.core :as bencode])) (symbol name-space) 'ns)
-   "(defn -main [& *command-line-args*] " main-file " )"
-))
 
-(println "making deps file")
-(spit "deps.edn" (str deps-file))
+(defn specific-require-generation []
+  (spit src-output-path
+    (str
+      (apply list (remove nil?
+        (list 'ns (symbol name-space)
+          '(:gen-class)
+           (specific-dependencies dependency-keys))))
+      "(defn -main [& *command-line-args*] " main-file " )"
+  ))
 
-(println "compiling native image")
-(shell/sh "clj" "-A:native-image")
+  (println "making deps file")
+  (spit "deps.edn" (str deps-file))
+
+  (println "compiling native image")
+  (shell-command compile-to-native-command)
+)
+
+(defn main []
+  (specific-require-generation)
+  (if-not (:keep-files options)
+   (do
+     (println "removing source file")
+     (delete-file src-output-path)
+     (println "removing deps.edn file")
+     (delete-file "deps.edn"))))
+
+(main)
